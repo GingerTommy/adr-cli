b86dc196472982fc3adeeb748c17e4bcfbbb7781
Thomas O'Connor <O'ConnorT@emea.cshare.net>
2017-07-02 10:29:45 +0100

    Start on the command line options

---
+++ src/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace src
+{
+    internal static class Program
+    {
+        private const string HelpOption = "-?|-h|--help";
+
+        private static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.Name = "adr-cli";
+            app.Description = "";
+
+            app.HelpOption(HelpOption);
+
+            app.Command("init", (command) =>
+            {
+                command.Description = "Init it";
+                command.Argument("[directory]", "");
+                command.HelpOption(HelpOption);
+            });
+
+            app.OnExecute(() =>
+            {
+                app.ShowHelp();
+                return 0;
+            });
+            app.Execute(args);
+        }
+    }
+}
